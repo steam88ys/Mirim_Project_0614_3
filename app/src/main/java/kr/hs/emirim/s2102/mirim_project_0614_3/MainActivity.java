03d8c4731c99a0f474f55475c0886cf3ad1ac851
steam88ys <yssteam88@gmail.com>
2022-06-14 20:43:11 +0900

    setFlipInterval 사용해서 완성

--- app/src/main/java/kr/hs/emirim/s2102/mirim_project_0614_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2102/mirim_project_0614_3/MainActivity.java
@@ -5,31 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     ViewFlipper vFlip;
+    EditText editInterval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vFlip = findViewById(R.id.vflip);
-        Button btnPrev = findViewById(R.id.btn_prev);
-        Button btnNext = findViewById(R.id.btn_next);
+        editInterval = findViewById(R.id.edit_interval);
+        Button btnStart = findViewById(R.id.btn_prev);
+        Button btnStop = findViewById(R.id.btn_next);
+        Button btnSecond = findViewById(R.id.btn_second);
 
-        btnPrev.setOnClickListener(btnListener);
-        btnNext.setOnClickListener(btnListener);
+        btnStart.setOnClickListener(btnListener);
+        btnStop.setOnClickListener(btnListener);
+        btnSecond.setOnClickListener(btnListener);
+        vFlip.setFlipInterval(1000);
     }
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.btn_prev:
-                    vFlip.showPrevious();
+                    vFlip.startFlipping();
                     break;
                 case R.id.btn_next:
-                    vFlip.showNext();
+                    vFlip.stopFlipping();
+                    break;
+                case R.id.btn_second:
+                    int second = Integer.parseInt(editInterval.getText().toString());
+                    vFlip.setFlipInterval(second*1000);
                     break;
             }
         }
